261b994b8a42377c5947c8f90c973bcd24cb43ba
Quentin <Quentin2008@gmail.com>
2025-03-26 11:53:58 +0100

    add oop-06 et oop-07

---
+++ 01-TheField/02-Python/02-PythonAdvanced/01-OOP/07-oop-nfs-2d.py
@@ -0,0 +1,70 @@
+
+class Engine():
+    def __init__(self, speed=0): ## engine ## input = speed (z, s)
+        self.speed = speed ## on peut remplacer speed par 0 et enlever speed=0 de l'init
+        self.position = [0,0]
+
+    def faster(self):
+        if self.speed < 9:
+            self.speed += 1
+
+    def slower(self):
+        if self.speed > 1:    
+            self.speed -= 1
+
+class Wheels(): ## wheels ## rotation grid ## input = rotation (q, d)
+
+    def rotate_left(self):
+        self.direction += 1 
+
+    def rotate_right(self):
+        self.direction += 3 ## -=1
+
+class Car(Engine,Wheels): ## main class
+    def __init__(self,name,color): ## can add speed=0
+        super().__init__() ## need to add speed if we add speed=0
+        self.name = name
+        self.color = color
+        self.direction = 1 # ["positif_y", "negatif_x", "negatif_y", "positif_x"]
+
+    def move(self):
+        if self.direction %4 == 1: # positif_y
+            self.position[1] += self.speed
+             
+        elif self.direction %4 == 2: # negatif_x
+            self.position[0] -= self.speed
+
+        elif self.direction %4 == 3: # negatif_y
+            self.position[1] -= self.speed
+
+        elif self.direction %4 == 0: # positif_x
+            self.position[0] += self.speed
+
+
+## app ## hybride nfs, snake ##
+## app ## hybride nfs, snake ##
+## app ## hybride nfs, snake ##
+
+snake_food = [7,2] ## add random.randint(), random X, random Y, add new food ## snake
+color = "blue" # input("choose a color for your car: ")
+name = "test" # input("choose a name for your car: ") 
+user_car = Car(name,color)
+
+print(f"you're goal is to reach: {snake_food}")
+print("--- use 'z' to accelerate -----")
+print("--- use 's' to decelerate -----")
+print("--- use 'q' to rotate left ----")
+print("--- use 'd' to rotate right ---")
+
+while user_car.position != snake_food: ## add time(), auto move each second
+    print(f"you're goal is to reach: {snake_food}")
+    print(f"Current position: {user_car.position}")
+
+    command = input("Enter command: ").strip()
+    if command == "z": user_car.faster()
+    elif command == "s": user_car.slower()
+    elif command == "q": user_car.rotate_left()
+    elif command == "d": user_car.rotate_right()
+    user_car.move()
+
+print(f"Reached the target grid position {snake_food}!")
